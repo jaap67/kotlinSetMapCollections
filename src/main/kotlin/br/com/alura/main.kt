f9be2ffd70cc206e8aa97ca64a8a0d1e60534701
Arlan Linux <marinhomoraisster@gmail.com>
2021-01-31 17:36:01 -0300

    kotlin - Set & Map - 01 - Tipos de coleção

--- src/main/kotlin/br/com/alura/main.kt
+++ src/main/kotlin/br/com/alura/main.kt
@@ -2,12 +2,32 @@ package br.com.alura
 
 fun main() {
 
-    val nomes: List<String> = listOf(
-        "Alex",
-        "Fran",
-        "Gui",
-        "Maria",
-        "Ana"
+    val banco = BancoDeNomes()
+//    banco.nomes.add("Alex")
+    banco.salva("Alex")
+    println(banco.nomes)
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes {
+    val nomes: Collection<String> get() = dados
+
+    fun salva(nome: String) {
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    } // Aqui deixamos apenas uma única instãncia do objeto, fazendo com que seja única no programa inteiro.
+}
+
+fun testaColecao() {
+    val nomes: Collection<String> = mutableListOf(
+            "Alex",
+            "Fran",
+            "Gui",
+            "Maria",
+            "Ana"
     )
     for (nome in nomes) {
         println(nome)
