55fb24dc15d06aa2ddf4450231c8b9e672aee6e5
Arlan Linux <marinhomoraisster@gmail.com>
2021-02-02 22:55:20 -0300

    kotlin - Set & Map - 04 - Escrita específica de Map

--- src/main/kotlin/br/com/alura/main.kt
+++ src/main/kotlin/br/com/alura/main.kt
@@ -7,7 +7,7 @@ fun main() {
             Pair(2, 64.0),
             3 to 50.0,
             4 to 100.0,
-            5 to 150.0,
+            5 to 100.0,
             6 to 80.0
     )
 
@@ -43,5 +43,30 @@ fun main() {
 
     println(pedidos.filterValues { it -> it > 70.0 }) // Pedidos acima de 70
     println(pedidos.filterKeys { it -> it % 2 == 0 }) // Pedidos pares
-}
 
+    println(pedidos + Pair(7, 43.0))
+    println(pedidos + mapOf(7 to 43.0, 8 to 99.0))
+    println(pedidos) // Não altera o map original, quando se utiliza o plus ou minus
+
+    println(pedidos - 6) // Remove o item 6 da lista e também não altera o map original
+    println(pedidos)
+
+    println(pedidos - listOf(6, 5)) // Pode ser removido uma lista de itens de uma só vez.
+    println(pedidos)
+
+//    pedidos.putAll(listOf<Pair<Int, Double>>(7 to 90.0, 8 to 20.0, 8 to 30.0))
+    pedidos += listOf<Pair<Int, Double>>(7 to 90.0, 8 to 20.0, 8 to 30.0) // Aqui utilizando o plusAssign (+=) operator
+    println(pedidos)
+
+    pedidos.keys.remove(1)
+    println(pedidos)
+
+    pedidos.values.remove(50.0)
+    println(pedidos)
+
+    pedidos.values.remove(100.0)
+    println(pedidos)
+
+    pedidos -= 6
+    println(pedidos)
+}
